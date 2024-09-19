0150b870d3e598b84fa64dc1795fab459dcd1325
성원 정 <tyhg56@naver.com>
2024-09-19 12:28:29 +0900

    Fix : " 유저 회원가입, 로그인 추가 "
    users.router.js 수정
     - usersRouter.post('/Sign-Up') ( 회원가입 )
    
     - usersRouter.post('/Sign-In') ( 로그인 )
    
    tokenCreate.js 추가 ( 토큰 생성 )
     - CreateAccessToken(id)
      - id를 받아 AccessToken 생성
    
     - CreateRefreshToken(id)
      - id를 받아 RefreshToken 생성
    
     - ValidateToken(token, secretkey)
      - token 검사

---
+++ utils/token/tokenCreate.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export function CreateAccessToken(id) {
+    const accessToken = jwt.sign(
+        { id: id },
+        process.env.ACCESS_TOKEN_SECRET_KEY,
+        { expiresIn: '600s' }
+    );
+
+    return process.env.TOKEN_TYPE + accessToken;
+}
+
+export function CreateRefreshToken(id) {
+    const refreshToken = jwt.sign(
+        { id: id },
+        process.env.REFRESH_TOKEN_SECRET_KEY,
+        { expiresIn: '7d' },
+    );
+
+    return process.env.TOKEN_TYPE + refreshToken;
+}
+
+export function ValidateToken(token, secretkey) {
+    try {
+        const payload = jwt.verify(token, secretkey);
+        return payload;
+    }
+    catch (error) {
+        return null;
+    }
+}
