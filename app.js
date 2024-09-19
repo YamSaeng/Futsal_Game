c0892d80f52c6d9f62fb517cb58118745904abc4
NamcheolHer <yihs97@naver.com>
2024-09-19 15:49:22 +0900

    Feat : DB 선수 정보 조회(전체/단일)
    DB 선수 정보 조회하기 추가

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ import express from "express";
 import UpgradeRouter from "./routes/upgrade.router.js";
 import GameStartRouter from "./routes/gameStart.router.js";
 import SquadRouter from "./routes/squad.router.js";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import UserRouter from "./routes/users.router.js";
+import characterRouter from "./routes/characters.router.js";
 
 const app = express();
 const PORT = 3000;
@@ -11,7 +12,13 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/FutsalGame", [UserRouter, UpgradeRouter, GameStartRouter, SquadRouter]);
+app.use("/FutsalGame", [
+  UserRouter,
+  characterRouter,
+  UpgradeRouter,
+  GameStartRouter,
+  SquadRouter,
+]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
