f314436f64029861a2af3b6105e5bb3ba8277b38
성원 정 <tyhg56@naver.com>
2024-09-19 15:00:44 +0900

    Fix : " UserRouter 등록 추가 "
    app.js 수정
     - cookieparser 등록
     - userRouter 등록

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ import express from "express";
 import UpgradeRouter from "./routes/upgrade.router.js";
 import GameStartRouter from "./routes/gameStart.router.js";
 import SquadRouter from "./routes/squad.router.js";
+import cookieParser from 'cookie-parser';
+import UserRouter from "./routes/users.router.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.use("/FutsalGame", [UpgradeRouter, GameStartRouter, SquadRouter]);
+app.use("/FutsalGame", [UserRouter, UpgradeRouter, GameStartRouter, SquadRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
