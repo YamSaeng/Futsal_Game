57864d1ef5e591af71b7a7af5ca282daded95bc7
성원 정 <tyhg56@naver.com>
2024-09-20 21:05:05 +0900

    Feat : " 시간 차 계산 추가 "
    time.js 수정
     - TimeDifference 추가
      - 시간 차 계산해서 반환

--- utils/time/time.js
+++ utils/time/time.js
@@ -7,12 +7,28 @@ export function MakeTime(isUTC, day = 0, hour = 0, minute = 0, second = 0) {
 
     if (isUTC == false) {
         nowTime.setHours(nowTime.getHours() + parseInt(process.env.LOCAL_TIME));
-    }    
+    }
 
     nowTime.setDate(nowTime.getDate() + day);
     nowTime.setHours(nowTime.getHours() + hour);
     nowTime.setMinutes(nowTime.getMinutes() + minute);
-    nowTime.setSeconds(nowTime.getSeconds() + second);        
+    nowTime.setSeconds(nowTime.getSeconds() + second);
 
     return nowTime;
+}
+
+// 시간 차 계산해서 format에 따라 반환
+export function TimeDifference(time, format) {
+    const nowTime = new Date();    
+
+    switch (format) {
+        case "day":
+            return Math.floor(Math.floor((time - nowTime) / 1000) / 86400);
+        case "hour":
+            return Math.floor(Math.floor((time - nowTime) / 1000) / 3600);
+        case "minute":
+            return Math.floor(Math.floor((time - nowTime) / 1000) / 60);
+        case "second":
+            return Math.floor((time - nowTime) / 1000);
+    }
 }
