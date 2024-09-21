3cc66a419c7cd0030c3c87128d217192eb88bb88
이상민 <yihs97@naver.com>
2024-09-21 15:46:48 +0900

    Fix : " inventory, character router 수정 "
    inventory router에 promise all 반영

--- routes/characters.router.js
+++ routes/characters.router.js
@@ -1,39 +1,39 @@
-import express from "express";
-import { prisma } from "../utils/prisma/prismaClient.js";
+import express from 'express';
+import { prisma } from '../utils/prisma/prismaClient.js';
 
 const CharactersRouter = express.Router();
 
 // DB 선수 전체 정보 조회
-CharactersRouter.get("/Character/Check", async (req, res, next) => {
+CharactersRouter.get('/Character/Check', async (req, res, next) => {
   try {
-    const Check = await prisma.CharacterDB.findMany();
+    const Check = await prisma.characterDB.findMany();
     return res.status(200).json({ Character_info: Check });
   } catch (error) {
-    res.status(500).json({ error: "선수 목록 조회에 실패했어요" });
+    res.status(500).json({ error: '선수 목록 조회에 실패했어요' });
     console.log(error);
   }
 });
 
 // DB 특정 선수 정보 조회
 CharactersRouter.get(
-  "/Character/Check/:characterDBId",
+  '/Character/Check/:characterDBId',
   async (req, res, next) => {
     try {
       const characterDBId = req.params.characterDBId; // 코드는 URI의 parameter로 전달 받기
 
-      const findCharacter = await prisma.CharacterDB.findUnique({
+      const findCharacter = await prisma.characterDB.findUnique({
         where: {
           characterDBId: +characterDBId, // 앞에 parseInt 또는 + 붙이면 숫자로 바뀐다.
         },
       });
-      if (findCharacter === null) {
-        res.status(404).json({ error: "존재하지 않는 선수 정보 입니다." });
+      if (!findCharacter) {
+        res.status(404).json({ error: '존재하지 않는 선수 정보 입니다.' });
         return;
       }
 
       res.status(200).json({ Character_info: findCharacter });
     } catch (error) {
-      res.status(500).json({ error: "선수 조회에 실패했어요" });
+      res.status(500).json({ error: '선수 조회에 실패했어요' });
       console.log(error);
     }
   }
