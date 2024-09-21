94fa3866e7989781c0bcab34a47a2b40f2db5163
성원 정 <tyhg56@naver.com>
2024-09-21 20:12:19 +0900

    Feat : " 전체 랭킹 조회, 특정 유저 랭킹 조회 추가 "
    ranking.router.js 수정
     - rankingRouter.get('Ranking/Check' ) 추가
      - 전체 랭킹 조회
    
     - rankingRouter.get('Ranking/Check/:userId) 추가
      - 특정 유저 랭킹 조회

--- routes/ranking.router.js
+++ routes/ranking.router.js
@@ -3,4 +3,85 @@ import { prisma } from '../utils/prisma/prismaClient.js'
 
 const rankingRouter = express.Router();
 
+let previousRankings = {};
+
+rankingRouter.get('/Ranking/Check', async (req, res, next) => {
+    // rankingScore 컬럼을 기준으로 내림차순한 전체 데이터 값들을 가져옴
+    const currentRankings = await prisma.ranking.findMany({
+        orderBy: {
+            rankingScore: 'desc'
+        }
+    });
+
+    // 순서대로 순회하며 랭킹 설정
+    let rank = 1;
+    currentRankings.map((currentRanking) => {
+        currentRanking.rank = rank;
+        rank++;
+    });
+
+    // ranking Table에 업데이트
+    await Promise.all(
+        currentRankings.map(async (currentRanking) => {
+            await prisma.ranking.update({
+                where: {
+                    userId: currentRanking.userId
+                },
+                data: {
+                    rank: currentRanking.rank
+                }
+            })
+        })
+    );
+        
+    // 순위 변동 계산
+    for(let pRankKey in previousRankings)
+    {
+        const pRankData = previousRankings[pRankKey];
+        for(let cRankKey in currentRankings)
+        {
+            const cRankData = currentRankings[cRankKey];
+            if(pRankData.userId == cRankData.userId)
+            {
+                const rankChangeScore = pRankData.rank - cRankData.rank;
+
+                cRankData.rankChangeScore = rankChangeScore;
+                break;
+            }            
+        }
+    }
+
+    // 순위 변동을 DB에 업데이트
+    await Promise.all(
+        currentRankings.map(async (currentRanking) => {
+            await prisma.ranking.update({
+                where: {
+                    userId: currentRanking.userId
+                },
+                data: {
+                    rankChangeScore: currentRanking.rankChangeScore
+                }
+            })
+        })
+    );
+    
+    // 현재 시점 순위 변동 저장
+    previousRankings = currentRankings;
+
+    // 순위 변동 결과 반환
+    return res.status(200).json({ currentRankings });
+});
+
+rankingRouter.get('/Ranking/Check/:userId', async (req, res, next) => {
+    const { userId } = req.params;
+
+    const userRanking = await prisma.ranking.findFirst({
+        where: {
+            userId: +userId
+        }
+    })
+
+    return res.status(200).json({ userRanking });
+});
+
 export default rankingRouter;
