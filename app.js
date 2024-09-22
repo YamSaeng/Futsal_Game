d75a826551ef13f7b34156adf78151ab0ab5c3e6
purebver <wkddj1541@gmail.com>
2024-09-22 17:17:34 +0900

    Design: "express.static"
    html을 사용하기위한 스태틱 메서드 추가

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-import express from "express";
-import UpgradeRouter from "./routes/upgrade.router.js";
-import GameStartRouter from "./routes/gameStart.router.js";
-import SquadRouter from "./routes/squad.router.js";
-import cookieParser from "cookie-parser";
-import UserRouter from "./routes/users.router.js";
-import characterRouter from "./routes/characters.router.js";
-import rankingRouter from "./routes/ranking.router.js";
+import express from 'express';
+import UpgradeRouter from './routes/upgrade.router.js';
+import GameStartRouter from './routes/gameStart.router.js';
+import SquadRouter from './routes/squad.router.js';
+import cookieParser from 'cookie-parser';
+import UserRouter from './routes/users.router.js';
+import CharacterRouter from './routes/characters.router.js';
+import RankingRouter from './routes/ranking.router.js';
+import InventoryRouter from './routes/inventory.router.js';
 
 const app = express();
 const PORT = 3000;
@@ -13,15 +14,22 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/FutsalGame", [
+app.use('/Signin', express.static('./http/Signin.html'));
+
+app.use('/Signup', express.static('./http/Signup.html'));
+
+app.use(express.static('./http'));
+
+app.use('/FutsalGame', [
   UserRouter,
-  characterRouter,
+  CharacterRouter,
   UpgradeRouter,
   GameStartRouter,
   SquadRouter,
-  rankingRouter
+  RankingRouter,
+  InventoryRouter,
 ]);
 
 app.listen(PORT, () => {
-  console.log(PORT, "포트로 서버가 열렸어요!");
+  console.log(PORT, '포트로 서버가 열렸어요!');
 });
